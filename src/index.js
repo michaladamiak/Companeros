74659700439ad15720757071c6c2950ac3190e32
michaladamiak <mchadamiak@gmail.com>
2022-09-16 12:33:01 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     
-    function scrollWin() {  window.scrollTo(0, 0);}
+//     function scrollWin() {  window.scrollTo(0, 0);}
 
     // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
     let vh = window.innerHeight * 0.01;
